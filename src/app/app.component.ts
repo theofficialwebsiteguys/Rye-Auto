911b55564faab8b4b5c73a322084694eba1b9b8a
PatrickDwyer <patricktdwyer01@gmail.com>
2024-02-27 00:09:07 -0500

    02.27.2024: Adding social bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SocialBarComponent } from './social-bar/social-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SocialBarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Template';
+  title = 'HitchCock HardScapes';
 }
---
+++ src/app/social-bar/social-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-bar.component.html',
+  styleUrl: './social-bar.component.css'
+})
+export class SocialBarComponent {
+
+}
