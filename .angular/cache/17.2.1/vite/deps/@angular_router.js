c10c2b22a22182ded3ba87c8a2f392a05906b668
PatrickDwyer <patricktdwyer01@gmail.com>
2024-03-02 16:42:23 -0500

    03.02.2024: Added contact and testimonials

--- .angular/cache/17.2.1/vite/deps/@angular_router.js
+++ .angular/cache/17.2.1/vite/deps/@angular_router.js
@@ -1,13 +1,15 @@
 import {
+  Title
+} from "./chunk-PKROL754.js";
+import {
   DOCUMENT,
   HashLocationStrategy,
   LOCATION_INITIALIZED,
   Location,
   LocationStrategy,
   PathLocationStrategy,
-  Title,
   ViewportScroller
-} from "./chunk-N66RCRT4.js";
+} from "./chunk-XXZCW4MP.js";
 import {
   APP_BOOTSTRAP_LISTENER,
   APP_INITIALIZER,
@@ -110,7 +112,7 @@ import {
   ɵɵloadQuery,
   ɵɵqueryRefresh,
   ɵɵsanitizeUrlOrResourceUrl
-} from "./chunk-B5LUOZ3Q.js";
+} from "./chunk-SFASAAZA.js";
 
 // node_modules/@angular/router/fesm2022/router.mjs
 var PRIMARY_OUTLET = "primary";
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 
+import { ContactComponent } from '../contact/contact.component';
+import { TestimonialsComponent } from '../testimonials/testimonials.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactComponent,
+    TestimonialsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  
+
 }
