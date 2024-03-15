5785c1c611e240954195726131b1797f96677b26
PatrickDwyer <patricktdwyer01@gmail.com>
2024-03-14 21:37:19 -0400

    03.14.2024: changed features and fixed the navbar

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 
