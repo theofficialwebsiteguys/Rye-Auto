f4c8f178c43622dfea72a17657991d56d977c432
PatrickDwyer <patricktdwyer01@gmail.com>
2024-03-15 00:08:15 -0400

    03.15.2024: Changed features to have slider to show before and after

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
-import { FormsModule } from '@angular/forms'; // Add this line
+
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
-export class FeaturesComponent {
-  sliderValue = 50;
+export class FeaturesComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const container1 = document.querySelector('.container') as HTMLElement;
+    const slider1 = document.querySelector('.slider') as HTMLInputElement;
+    slider1.addEventListener('input', (e) => {
+      const target = e.target as HTMLInputElement;
+      container1.style.setProperty('--position', `${target.value}%`);
+    });
+
+    const container2 = document.querySelector('.container2') as HTMLElement;
+    const slider2 = document.querySelector('.slider2') as HTMLInputElement;
+    slider2.addEventListener('input', (e) => {
+      const target = e.target as HTMLInputElement;
+      container2.style.setProperty('--position', `${target.value}%`);
+    });
+
+    const container3 = document.querySelector('.container3') as HTMLElement;
+    const slider3 = document.querySelector('.slider3') as HTMLInputElement;
+    slider3.addEventListener('input', (e) => {
+      const target = e.target as HTMLInputElement;
+      container3.style.setProperty('--position', `${target.value}%`);
+    });
+  }
 }
